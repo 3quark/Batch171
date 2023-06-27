7ee1efb4f9fec7defc717babd126257a9351f858
kaan <kaankamiloglu@gmail.com>
2023-06-27 14:00:08 +0300

    v3

---
+++ src/main/java/day02datatypesmethodcreation/Variables02.java
@@ -0,0 +1,53 @@
+package day02datatypesmethodcreation;
+
+public class Variables02 {
+    /*
+    Java'da temelde ik tip data vardir.
+    1). primitive data type:
+    char, boolean,byte,short,int,long,float,double
+    2).non-primitive data type:
+    String...
+
+     */
+    /*
+    note1: primitive data typlerini  java olusturmustur. biz olusturamayiz.
+    note2:primitive data typle'rinin isimlerinde sadece kucuk harf kullanilir.
+    note3:primitive data type'larina gore memory defarkli farkli yer kaplarlar.
+    note4:primitive datalar iclerinde sadece sizin atadiginiz degeri barindirirlar.
+
+     */
+
+    /*
+    non-primitive data types
+    ornegin string non-primitive data typedir.
+     buyuzden "non-primitive" data type'lar sinirsiz sayidadir denilebilir.
+     *non-primitive data type'larin isimleri buyuk harfle baslar.
+     *non-primitive ler icin java memory de buyuklugune gore degisen boyutlarda yer ayirir.
+     */
+    public static void main(String[] args) {
+        //ornek1: ulke ismi icin bir variable olusturun ve bir deger  atayip consol'a ayzdirin
+        String ulkeISmi="Turkiye";
+        System.out.println(ulkeISmi);
+      /*
+
+        Soru  primitive ve non-primitive data type lari arasindaki farklar nelerdir?
+        1) "primitive" ler sadece bizim atadigimiz degeri icerir
+            "non-primitive" ler bizim atadigimiz degeri ve methodlari icerir
+        2) "primitive" ler kucuk harfle baslar, "non-primitive" ler buyuk harfle baslar
+        3) "primitive" leri java uretmistir ve 8 tanedir
+           "non-primitive" leri java ve developerlar uretebilir, sinirsiz sayidadir.
+        4) "primitive" ler memory de data type larina gore sabit boyutta bellek kullanir
+           "non-primitive" ler icin java memory de buyuklugune gore degisen boyutlarda bellek kullanabilirler
+
+
+       */
+
+
+
+
+
+
+
+
+    }
+}
